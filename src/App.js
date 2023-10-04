910cf7b245948be5c6aaaf47a85a8c378c292d5b
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-10-04 13:40:30 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -7,13 +7,23 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  
+  
+useEffect(() => {
+ const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+ if (storedUserLoggedInInformation === "1") {
+   setIsLoggedIn(true);
+ }
+}, []);
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
