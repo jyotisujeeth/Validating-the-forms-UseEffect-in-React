9f38aa123e3da059b99e6bf7f594316d38938803
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-10-05 06:23:33 +0530

    form validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-setFormIsValid(
-  enteredEmail.includes("@") && enteredPassword.trim().length > 6
-);
-  },[ enteredEmail, enteredPassword]);
+    const identifier = setTimeout(() => {
+      console.log('checking form validation ');
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+      return () => {
+        console.log(")............(");
+        clearTimeout(identifier);
+      };
+    }, [enteredEmail, enteredPassword]);},500);
+
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
